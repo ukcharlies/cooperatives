77e801eee7b18f130fba1758d5dadd8d47aed2c4
Oyindamola .P. Dosumu <introvertllc2019@gmail.com>
2024-07-09 11:24:31 +0100

    fixed urls.py

--- aui_cooperatives/api/urls.py
+++ aui_cooperatives/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import register_user, login_user
+from .views import register_user, login_user, get_user
 
 urlpatterns = [
     path("register/", register_user, name="register_user"),
     path("login/", login_user, name="login_user"),
+    path("user/", get_user),
 ]
